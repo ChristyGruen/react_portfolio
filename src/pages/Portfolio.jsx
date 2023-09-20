f49fd245203fd60a675c0c809e55712bfb12c098
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-09-20 13:09:59 -0500

    update portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,12 @@
 
 import Card from 'react-bootstrap/Card';
 import TechBlogImage from '../assets/TechBlog.jpg';
-import lakeimage from '../assets/IMG_9493.jpg';
+import CalendarAppImage from '../assets/calendar_data_retrieval.jpg';
+import WeatherAppImage from '../assets/weather_app.png';
+import CodingQuizImage from '../assets/codingQuiz.png';
+import PWGeneratorImage from '../assets/PW_Generator.png';
+import SudokuImage from '../assets/Screenshot-Sudoku.png'
+
 
 export default function Portfolio(){
 
@@ -16,73 +21,131 @@ export default function Portfolio(){
         <Card.Title>Tech Blog</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Handlebars, Sequelize, Express, Heroku</Card.Subtitle>
         <Card.Img variant = 'bottom' src="TechBlog.jpg" />
-        <Card.Link href="#">Github Repo</Card.Link>
+        <Card.Text>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>          
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </Card.Text>
+        <Card.Link href="https://github.com/ChristyGruen/tech_blog">gitHub repo</Card.Link>
         <Card.Link href="https://tech-blog-cgru-a07b953be274.herokuapp.com/">Heroku App</Card.Link>
       </Card.Body>
       
     </Card>
-    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${lakeimage})` }}>
+    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${CalendarAppImage})` }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>Calendar App</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">HTML, CSS, JQuery</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>          
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="https://github.com/ChristyGruen/calendar_app">gitHub repo</Card.Link>
+        <Card.Link href="https://christygruen.github.io/calendar_app/">Calendar App</Card.Link>
       </Card.Body>
     </Card>
     </row>
     <row className = 'd-flex'>
-    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${TechBlogImage})` }}>
+    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${WeatherAppImage})` }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>Weather App</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Dashboard, HTML, CSS, API</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <br></br>
+          <br></br>
+          <br></br>
+          <br></br>          
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="https://github.com/ChristyGruen/weather_app">gitHub repo</Card.Link>
+        <Card.Link href="https://christygruen.github.io/weather_app/">Weather App</Card.Link>
       </Card.Body>
     </Card>
-    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${lakeimage})` }}>
+    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${CodingQuizImage})` }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>Coding Quiz</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">JavaScript, HTML, CSS</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <br></br>
+          <br></br>
+          <br></br>
+          <br></br>          
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="https://github.com/ChristyGruen/coding_quiz">gitHub repo</Card.Link>
+        <Card.Link href="https://christygruen.github.io/coding_quiz/">Coding Quiz</Card.Link>
       </Card.Body>
     </Card>
     </row>
     <row className = 'd-flex'>
-    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${TechBlogImage})` }}>
+    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${PWGeneratorImage})` }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>Password Generator</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">JavaScript, CSS, HTML</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <br></br>
+          <br></br>
+          <br></br>
+          <br></br>          
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="https://github.com/ChristyGruen/password_generator">gitHub repo</Card.Link>
+        <Card.Link href="https://christygruen.github.io/password_generator/">Password Generator</Card.Link>
       </Card.Body>
     </Card>
-    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${lakeimage})` }}>
+    <Card className = 'm-2 p-2'style={{ height: '25rem', width: '30rem', backgroundImage: `url(${SudokuImage})` }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>Sudoku</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Interactive game, JavaScript, HTML, CSS</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <br></br>
+          <br></br>
+          <br></br>
+          <br></br>          
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="https://github.com/ChristyGruen/fantastic-octo-sudoku">gitHub repo</Card.Link>
+        <Card.Link href="https://ad31aid.github.io/fantastic-octo-sudoku/">Sudoku</Card.Link>
       </Card.Body>
     </Card>
     </row>
