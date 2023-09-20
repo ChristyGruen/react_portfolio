c4ea1f864b09f0354b36ba1104c46b43b7530efb
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-09-20 15:19:45 -0500

    fixed background image issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ export default function Home(){
   
   return(
     <>
+    <div className = 'center-background rounded-4'>
     <h2>About Me</h2>
-    <p className= 'text-dark'>
-    Dedicated science professional with extensive experience in data analysis focused on medical device development, technical operations, and production. Passionate about utilizing product and customer data to support data-driven decision making. Motivated by teams of cross-functional experts working together to solve multi-faceted problems.
-    </p>
+    <p >
 
+    Dedicated science professional with extensive experience in data analysis focused on medical device development, technical operations, and production. Passionate about utilizing product and customer data to support data-driven decision making. Motivated by teams of cross-functional experts working together to solve multi-faceted problems.
+  
+    </p>
+    </div>
     </>
   )
 }
